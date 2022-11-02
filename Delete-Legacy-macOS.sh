6e0bec8c1b06aa506fb14f2fe7427046a10e134d
SamStar <54240024+Samstar777@users.noreply.github.com>
2022-11-02 13:14:42 +0530

    Add files via upload

---
+++ Delete-Legacy-macOS.sh
@@ -0,0 +1,41 @@
+﻿#!/bin/bash
+#
+# Created By: Salim Ukani
+# Date: Feb 15, 2022.
+# Description: Script to seacrh and delete the specified macOS installers.
+#
+# List of legacy macOS installers to be removed (Add/Remove if necessary)
+macOSLegacyInstallers=(
+	/Applications/Install\ macOS\ High\ Sierra.app
+	/Applications/Install\ macOS\ Mojave.app
+    /Applications/Install\ macOS\ Catalina.app
+    /Applications/Install\ macOS\ Big\ Sur.app
+)
+
+# Function to delete
+function deleteItems()
+{
+	declare -a toDelete=("${!1}")
+
+	for item in "${toDelete[@]}"
+		do
+			if [[ ! -z "${2}" ]]
+				then
+					item=("${2}""${item}")
+			fi
+
+			echo "Looking for $item"
+
+			if [ -e "${item}" ]
+				then
+					echo "Legacy macOS installer Found: Removing $item"
+					rm -rf "${item}"
+			fi
+		done
+}
+
+
+# Delete legacy installers
+deleteItems macOSLegacyInstallers[@]
+
+exit 0
